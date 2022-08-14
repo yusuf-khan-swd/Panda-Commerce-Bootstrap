cf25dbc5386db9936be38e0e14b3ad1341a060f0
yusuf-khan-swd <yusufkhan.swd@gmail.com>
2022-08-15 01:09:01 +0600

    adding javascritp to the code

---
+++ js/index.js
@@ -0,0 +1,6 @@
+
+const elements = document.getElementsByTagName('h2');
+for (let element of elements)
+  element.style.color = 'lightblue';
+
+document.getElementById('backpack').style.backgroundColor = 'tomato';
