3af9dddb256204bc9c56d589b17da74e2eb9164a
yusuf-khan-swd <yusufkhan.swd@gmail.com>
2022-08-15 16:18:54 +0600

    add comment to dom script added addItem button

--- js/index.js
+++ js/index.js
@@ -1,30 +1,30 @@
 
-// Requirement 2
+// Requirement 2: select by element
 const elements = document.getElementsByTagName('h2');
 for (let element of elements) {
   element.style.color = 'lightblue';
 }
 
 
-// Requirement 3
+// Requirement 3: select by id
 document.getElementById('backpack').style.backgroundColor = 'tomato';
 document.getElementById('backpack').style.padding = '2rem';
 
 
-// Requirement 4
+// Requirement 4: select all classes
 const allCardClasses = document.getElementsByClassName('card');
 for (let card of allCardClasses) {
   card.style.borderRadius = '30px';
 }
 
 
-// Requirement 5
+// Requirement 5: add funtion to onclick event
 function myFunction() {
   console.log('Hello From JavaScript');
 }
 
 
-// Requirement 6
+// Requirement 6: remove buy now button on click
 const allBuyNowButtons = document.getElementsByClassName('panda-btn-buy-now');
 for (let button of allBuyNowButtons) {
   button.addEventListener('click', function (event) {
@@ -34,7 +34,7 @@ for (let button of allBuyNowButtons) {
 }
 
 
-// Requirement 7
+// Requirement 7: eamil
 const submitButton = document.getElementById('submit-button');
 const inputField = document.getElementById('input-field');
 inputField.addEventListener('keyup', function (event) {
@@ -47,7 +47,7 @@ inputField.addEventListener('keyup', function (event) {
 });
 
 
-// Requirement 10
+// Requirement 10: add event to mouseenter  and mouseleave
 function changeImage() {
   document.getElementById('image').style.transform = 'scale(1.2)';
   document.getElementById('image').style.transition = 'transform 0.3s'
@@ -58,7 +58,7 @@ function removeChange() {
 }
 
 
-// Requirement 9
+// Requirement 9: change background with double click
 function random(number) {
   return Math.floor(Math.random() * number);
 }
@@ -71,7 +71,7 @@ document.getElementById('subscribe').addEventListener('dblclick', function (even
 
 
 
-// Extra
+// Extra password matching
 const matchedButton = document.getElementById('matched');
 const password = document.getElementById('password');
 document.getElementById('confirm-password').addEventListener('keyup', function (event) {
